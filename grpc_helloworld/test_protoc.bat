efbd6d7fa3858071e43dc5eef1dcb5de81a477e7
plasticbox <plasticbox@gmail.com>
2016-07-15 10:14:51 +0900

    Add grpc_helloworld

---
+++ grpc_helloworld/test_protoc.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pushd %~dp0
+
+set path=%cd%
+set output_client_path=%path%\src\client\
+set output_server_path=%path%\src\server\
+set protoc_path=%path%\..\grpc\bin\protobuf\release
+
+%protoc_path%\protoc.exe helloworld.proto --grpc_out=%output_client_path% --plugin=protoc-gen-grpc=%protoc_path%\grpc_cpp_plugin.exe --proto_path=.\
+%protoc_path%\protoc.exe helloworld.proto --grpc_out=%output_server_path% --plugin=protoc-gen-grpc=%protoc_path%\grpc_cpp_plugin.exe --proto_path=.\
+
+%protoc_path%\protoc.exe helloworld.proto --cpp_out=%output_client_path% --proto_path=.\
+%protoc_path%\protoc.exe helloworld.proto --cpp_out=%output_server_path% --proto_path=.\
